9730306b28dc86c7d95b2c0536743ab458a38018
Mohamed Radwan <128183682+MohamedRadwan10@users.noreply.github.com>
2025-02-09 17:44:54 +0200

    Update Result.jsx

--- src/Components/Result/Result.jsx
+++ src/Components/Result/Result.jsx
@@ -3,7 +3,7 @@ import Card from "../Card/Card";
 
 const Result = ({ results }) => {
   return (
-    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4 gap-5 p-5">
       {results.map((result) => {
         return (
           <Card key={result.id} result={result} />
